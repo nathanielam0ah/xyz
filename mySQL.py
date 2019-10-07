111dae8c5d0694ede6d6872d110bf010628bb797
nathanielam0ah <nathanielam0ah@github.com>
2019-10-07 15:33:39 +0000

    mySQL module

---
+++ mySQL.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+
+import mysql.connector
+from config import host,user,password
+
+mydb = mysql.connector.connect(
+  host=host,
+  user=user,
+  passwd=password,
+)
+
+mycursor = mydb.cursor()
+
+class manageDB:
+    def createDB (self, name):
+        mycursor.execute("CREATE DATABASE IF NOT EXISTS {}".format(name))
+
+    def createTB (self,database, name):
+        mycursor.execute("USE {}".format(database))
+        mycursor.execute("CREATE TABLE IF NOT EXISTS {} (links VARCHAR(255))".format(name))
+
+    def insertTB (self, database, table, html_links):
+        mycursor.execute("USE {}".format(database))
+        sqlFormula = "INSERT INTO {} (links) VALUES ('{}')".format(table, html_links)
+        mycursor.execute(sqlFormula, html_links)
+        mydb.commit()
